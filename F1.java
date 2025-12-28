a8a13a46015b7b50797c104eb270c42647b43a60
vaibhavpagar97 <94346668+vaibhavpagar97@users.noreply.github.com>
2025-12-28 22:36:37 +0530

    Create F1.java
    Take user input

---
+++ F1.java
@@ -0,0 +1,15 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class F3 {
+	public static void main (String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter age");
+		
+		int age = sc.nextInt();
+		System.out.println("My age is" + age);
+	}
+
+}
