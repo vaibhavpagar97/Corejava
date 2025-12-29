c642b9b09c855f7c47e78738837c88ce2b7ec78c
vaibhavpagar97 <94346668+vaibhavpagar97@users.noreply.github.com>
2025-12-29 21:22:20 +0530

    Create F4.java
    Use on Scanner class to take int input from user

---
+++ F4.java
@@ -0,0 +1,26 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class F4 {
+	
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Enter number 1");
+		
+		int num1 = sc.nextInt();
+		
+		System.out.println("Enter number 2");
+		
+		int num2 = sc.nextInt();
+		
+		int addition = num1 + num2 ;
+		
+		System.out.println("The addition of above two numbers is " + addition + ".");
+		
+		
+	}
+
+}
