f6eacdbbe6524f152f053409a005dee5acbc24bf
vaibhavpagar97 <94346668+vaibhavpagar97@users.noreply.github.com>
2025-12-30 22:56:59 +0530

    Create F7.java
    Replace two numbers

---
+++ F7.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class F7 {
+	
+	public static void main(String args[]) {
+	int c = 2, m=3;
+	int temp = c ;
+
+		c=m;
+		m=temp;
+		
+		System.out.println(c + "and" + m);
+	}
+}
